5c389fcbb169a9f4f73b372d4ced6a5aee932737
Tamae <t728532417114m@gmail.com>
2018-07-03 19:11:42 -0400

    front end config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/calendar.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
