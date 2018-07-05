7fb3c199249ff9c5fb8fed1a1cd4ff525331f9ed
Tamae <t728532417114m@gmail.com>
2018-07-05 16:23:13 -0400

    react router, app & root components

--- frontend/calendar.jsx
+++ frontend/calendar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import configureStore from './store/store'
-// import Root from './components/root';
+import Root from './components/root';
 // import { login, signup, logout } from './util/session_api_util';
 
 
@@ -33,6 +34,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // window.fetchCategory = fetchCategory;
 //TESTING END
 
-  // ReactDOM.render(<Root store={store} />, root);
-  ReactDOM.render(<h1>calendar</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 })
---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Calendar App from App Component</h1>
+    </div>
+  )
+};
+
+export default App;
